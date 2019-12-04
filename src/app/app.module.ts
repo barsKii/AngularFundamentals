6a9b5dd68673e6c629b1c8c460bda6297b36f425
David Balzer <david.balzer@fuel50.com>
2019-12-04 16:16:11 +1300

    Pushing at early staging of routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/event.service';
+import { ToastrService } from './common/toastr.service';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { appRoutes } from './routes';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    EventDetailsComponent,
+    EventThumbnailComponent,
+    EventsAppComponent,
+    EventsListComponent,
+    NavBarComponent
+  ],
+  providers: [
+    EventService,
+    ToastrService
+  ],
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
