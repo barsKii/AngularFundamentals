5c142011eaf54fdd3ebbc074b20a2bfa58854394
David Balzer <david.balzer@fuel50.com>
2019-12-06 13:04:56 +1300

    Created Interface for Events

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { IEvent } from './shared/index';
 
 @Component({
     // tslint:disable-next-line: component-selector
@@ -32,7 +33,7 @@ import { Component, Input } from '@angular/core';
     `]
 })
 export class EventThumbnailComponent {
-    @Input() event: any;
+    @Input() event: IEvent;
 
     getStartTimeClass() {
         if (this.event && this.event.time === '8:00 am') {
