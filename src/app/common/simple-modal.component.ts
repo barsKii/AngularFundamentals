8ea2df310823c4c57a11292feb524abbb576d086
David Balzer <david.balzer@fuel50.com>
2019-12-23 13:44:54 +1300

    Added ability to close modal when clicking on modal body

--- src/app/common/simple-modal.component.ts
+++ src/app/common/simple-modal.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, Inject } from '@angular/core';
+import { JQ_TOKEN } from './jQuery.service';
 
 @Component({
     // tslint:disable-next-line: component-selector
     selector: 'simple-modal',
     template: `
-    <div id="{{elementId}}" class="modal fade" tabindex="-1">
+    <div id="{{elementId}}" #modalcontainer class="modal fade" tabindex="-1">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -15,7 +16,7 @@ import { Component, Input } from '@angular/core';
                     </button>
                     <h4 class="modal-title">{{title}}</h4>
                 </div>
-                <div class="modal.body">
+                <div class="modal-body" (click)="closeModal()">
                     <ng-content></ng-content>
                 </div>
             </div>
@@ -30,4 +31,14 @@ import { Component, Input } from '@angular/core';
 export class SimpleModalComponent {
     @Input() title: string;
     @Input() elementId: string;
+    @Input() closeOnBodyClick: string;
+    @ViewChild('modalcontainer', {static: false}) containerEl: ElementRef;
+
+    constructor(@Inject(JQ_TOKEN) private $: any) {}
+
+    closeModal() {
+        if (this.closeOnBodyClick.toLocaleLowerCase() === 'true') {
+            this.$(this.containerEl.nativeElement).modal('hide');
+        }
+    }
 }
